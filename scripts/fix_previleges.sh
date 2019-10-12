8dd95c1c16c1016718f7def5f603da009e9b2f1c
Tamilamudhan Ramasamy <Tamilamudhan_R@ad.infosys.com>
2019-10-12 23:20:01 +0530

    update

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -2,3 +2,5 @@
 chmod +x /home/ec2-user/target/student-services-security-0.0.1-SNAPSHOT.war
 chmod +x /home/ec2-user/scripts/start_server.sh
 chmod +x /home/ec2-user/scripts/stop_server.sh
+
+
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 pkill -f 'java -jar'
