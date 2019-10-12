1ac5213e7d7d1e93d1d4aebdf168e74a3186956b
tamilamudhan <48781815+tamilamudhan@users.noreply.github.com>
2019-10-12 17:50:57 +0530

    Update fix_previleges.sh

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod +x /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ec2-user/target/student-services-security-0.0.1-SNAPSHOT.war
+chmod +x /home/ec2-user/scripts/start_server.sh
+chmod +x /home/ec2-user/scripts/stop_server.sh
