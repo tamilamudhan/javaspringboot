5d012ad76486afa00a25a51d01cf5669303571dc
tamilamudhan <48781815+tamilamudhan@users.noreply.github.com>
2019-10-16 14:49:06 +0530

    Update SecurityConfig.java

--- src/main/java/com/in28minutes/springboot/security/SecurityConfig.java
+++ src/main/java/com/in28minutes/springboot/security/SecurityConfig.java
@@ -7,20 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 
 @Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	// Authentication : User --> Roles
-	//protected void configure(AuthenticationManagerBuilder auth)
-		//	throws Exception {
-		//auth.inMemoryAuthentication().passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()).withUser("user1").password("secret1")
-				//.roles("USER").and().withUser("admin1").password("secret1")
-				//.roles("USER", "ADMIN");
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	 Authentication : User --> Roles
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()).withUser("user1").password("secret1")
+				.roles("USER").and().withUser("admin1").password("secret1")
+				.roles("USER", "ADMIN");
 	}
 
-	// Authorization : Role -> Access
-	//protected void configure(HttpSecurity http) throws Exception {
-	//	http.httpBasic().and().authorizeRequests().antMatchers("/students/**")
-	//			.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
-	//			.csrf().disable().headers().frameOptions().disable();
-//	}
+	 Authorization : Role -> Access
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/students/**")
+				.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
+				.csrf().disable().headers().frameOptions().disable();
+	}
 
 }
